e148a725c8675f4d3521d7c00d53b4f465cc0206
ShukranBabaev <marina300@bk.ru>
2022-05-05 13:59:14 +0300

    lesson 23 (исправление замечаний - поставил Транзакцию на метод buy)

--- src/main/java/com/example/operasales/service/OperaService.java
+++ src/main/java/com/example/operasales/service/OperaService.java
@@ -7,6 +7,7 @@ import com.example.operasales.repository.JPATicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Random;
 
@@ -45,6 +46,7 @@ public class OperaService {
         System.out.println(pr);
     }
 
+    @Transactional
     public Ticket buy(String name) {
 
         Random random = new Random();
